2be0a9453096f5f8aef71e38e632c8466ea0bd2a
Sudarsan <sudarsan.sivakumar@paroscale.com>
2025-07-23 13:25:29 +0000

    Include new struct for the function implementation

--- go/pkg/pumicefunc/common/funclib.go
+++ go/pkg/pumicefunc/common/funclib.go
@@ -1,6 +1,19 @@
 package funclib
 
+//Struct to forward from the pmdb client to the pmdb server
 type FuncReq struct {
     Name string
-    Args []interface{}
+    Args []byte
+}
+
+//Struct to contain KV changes
+type CommitChg struct {
+    Key   []byte
+    Value []byte
+}
+
+//Struct for sending the KV changes and Response to client from the WritePrep to Apply
+type FuncIntrm struct {
+    Changes []CommitChg
+    Response []byte
 }
