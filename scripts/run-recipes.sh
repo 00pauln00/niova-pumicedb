a54943942bb75d7ad7659648dc72894cec46ca68
HimaniMalani17 <himani.malani@paroscale.com>
2025-07-30 11:26:46 +0000

    Changed holon ref branch to test

--- scripts/run-recipes.sh
+++ scripts/run-recipes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+unset LD_PRELOAD
 HOLON_LIBS=${1}
 BIN_PATH=${2}
 LOG_PATH=${3}
@@ -26,7 +26,7 @@ do
    if [ $# -eq 7 ]
    then
       GO_PATH=${7}
-      ansible-playbook -e 'srv_port=4510' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e recipe=$recipe -e 'backend_type=pumicedb' -e app_name=$APP_TYPE holon.yml
+      ansible-playbook -e 'srv_port=4510' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e recipe=$recipe -e 'backend_type=pumicedb' -e app_type=$APP_TYPE holon.yml
    elif [ $# -eq 6 ]
    then
       NNISD=${6}
@@ -34,12 +34,12 @@ do
    elif [[ ( $# -eq 9 ) && $APP_TYPE == "controlplane" ]]
    then
       NCLIENTS=${8}
-      ansible-playbook -e 'srv_port=4510' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e recipe=$recipe -e app_name=$APP_TYPE -e nlookouts=$NLOOKOUT -e nnisds=$NNISD -e nclients=$NCLIENTS holon.yml
+      ansible-playbook -e 'srv_port=4510' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e recipe=$recipe -e app_type=$APP_TYPE -e nlookouts=$NLOOKOUT -e nnisds=$NNISD -e nclients=$NCLIENTS holon.yml
    else
       ENABLE_COALESCED_WR=${7}
       ENABLE_SYNC=${8}
       GO_PATH=${9}
-      ansible-playbook -e 'srv_port=4510' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e recipe=$recipe -e 'backend_type=pumicedb' -e app_name=$APP_TYPE -e coalesced_wr=$ENABLE_COALESCED_WR -e sync=$ENABLE_SYNC holon.yml
+      ansible-playbook -e 'srv_port=4510' -e npeers=$NPEERS -e dir_path=$LOG_PATH -e 'client_port=14000' -e recipe=$recipe -e 'backend_type=pumicedb' -e app_type=$APP_TYPE -e coalesced_wr=$ENABLE_COALESCED_WR -e sync=$ENABLE_SYNC holon.yml
    fi
 
    if [ $? -ne 0 ]
