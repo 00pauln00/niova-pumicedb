4de53d1d0de4b94968c0fa0a7662568bea2b3e66
Sudarsan <sudarsan.sivakumar@paroscale.com>
2025-10-13 05:23:57 +0000

    Bug fixes for containerization

--- scripts/docker/run-pmcontainer.sh
+++ scripts/docker/run-pmcontainer.sh
@@ -1,7 +1,8 @@
 #Provide start of the port range as command line arg
 #I.e sudo ./run-cpcontainer.sh 5000
 rm -rf *.raftdb configs ctl-interface logs
-./raft-configs.sh $1
+./raft-config.sh $1
+mkdir -p ctl-interface
 docker build -t pumice .
 curdir=$(pwd)
-docker compose up -d --build
+docker-compose up -d --build
