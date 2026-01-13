8302929d0e7b5310e02ff411d914e102efff63b1
Sudarsan <sudarsan.sivakumar@paroscale.com>
2026-01-13 09:04:32 +0000

    Update pumiceerr with better error messages

--- go/pkg/pumiceerr/pumiceerr.go
+++ go/pkg/pumiceerr/pumiceerr.go
@@ -14,13 +14,13 @@ var (
 	ErrRNCUIAlreadyQueued = errors.New("RNCUI already queued in the pumice client")
 
 	ErrInvalid         = errors.New("Invalid initialization")
-	ErrNoMem           = errors.New("No memory")
-	ErrTooBIG          = errors.New("Too big of a message")
+	ErrNoMem           = errors.New("No memory in pumice client/server")
+	ErrTooBIG          = errors.New("The application message is too big, expects 41,93,704 bytes max (4MB - 88 bytes - 512 bytes)")
 	ErrNoLeader        = errors.New("No pumice leader identified yet")
 	ErrNoSpace         = errors.New("Pumice client heap is full")
 	ErrAlreadyQ        = errors.New("Request already queued for the same RNCUI in the pumice client")
 	ErrTimeout         = errors.New("Operation timed out at the pumice client")
-	ErrTryAgain        = errors.New("Temporary failure in raft side, try again")
+	ErrTryAgain        = errors.New("Temporary failure in the raft side, try again")
 	ErrNoSpaceOnServer = errors.New("No space left on write supplement buffer on the pumice server")
 )
 
@@ -79,6 +79,8 @@ func TranslatePumiceServerOpErrCode(code int) error {
 	case -28:
 		//ENOSPC
 		return ErrNoSpaceOnServer
+	default:
+		return fmt.Errorf("unknown error code: %d", code)
 	}
 	return nil
 }
